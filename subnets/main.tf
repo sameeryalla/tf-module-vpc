14bf9a8c7991b8f6983182ece77787da69ddbcb2
sameeryalla <sameeryalla@gmail.com>
2023-05-31 23:43:54 +0530

    addded subnet records 3

--- subnets/main.tf
+++ subnets/main.tf
@@ -2,5 +2,5 @@ resource "aws_subnet" "main" {
   count=lengh(var.cidr_block)
   vpc_id = var.vpc_id
   cidr_block = var.cidr_block[count.index]
-  tags = merge(var.tags, { Name = "${var.env}-${var.name}-${count.index}" })
+  tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}" })
 }
