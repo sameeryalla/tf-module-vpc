4f5571dffccd65cecf0d211782251958fd46a755
sameeryalla <sameeryalla@gmail.com>
2023-06-01 21:23:48 +0530

    Added internet gateway to route table

--- subnets/main.tf
+++ subnets/main.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "main" {
   tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}" })
 }
 
-resource "aws_route_table" "example" {
+resource "aws_route_table" "main" {
   count =  length(var.cidr_block)
   vpc_id = var.vpc_id
 
@@ -16,5 +16,5 @@ resource "aws_route_table" "example" {
 resource "aws_route_table_association" "associate" {
   count     =  length(var.cidr_block)
   subnet_id = aws_subnet.main[count.index].id
-  route_table_id = aws_route_table.example[count.index].id
+  route_table_id = aws_route_table.main[count.index].id
 }
--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -1,3 +1,7 @@
 output "subnet_ids"{
   value=aws_subnet.main.*.id // * means list of ids
 }
+
+output "route_table_ids"{
+  value=aws_route_table.main.*.id // * means list of ids
+}
