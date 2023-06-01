8d8853d811dfbf7a2271eb0990a3e9e98dc0dcb8
sameeryalla <sameeryalla@gmail.com>
2023-06-01 20:55:10 +0530

    added route table

--- subnets/main.tf
+++ subnets/main.tf
@@ -5,3 +5,10 @@ resource "aws_subnet" "main" {
   availability_zone = var.azs[count.index]
   tags = merge(var.tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}" })
 }
+
+resource "aws_route_table" "example" {
+  count =  length(var.cidr_block)
+  vpc_id = var.vpc_id
+
+  tags = merge(var.tags, { Name = "${var.env}-${var.name}-rt-${count.index+1}"})
+}
